dc2bf96e5d0d362de5a246725d8b284084cc93c8
BarokDG <it.barok.dagim@gmail.com>
2021-10-03 13:54:32 +0300

    added data and dynamic rendering

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const app = new Vue({
   el: "#app",
   data: {
-    message: "Hello Barok!",
+    marker: "hair",
+    options: {
+      hair: ["bang", "curls", "default", "elegant", "fancy", "quiff", "short"],
+      ears: ["default", "tilt-backward", "tilt-forward"],
+      eyes: ["angry", "default", "naughty", "panda", "smart", "star"],
+      mouth: ["astonished", "default", "eating", "laugh", "tongue"],
+      neck: ["bend-backward", "bend-forward", "default", "thick"],
+      leg: [
+        "bubble-tea",
+        "cookie",
+        "default",
+        "game-console",
+        "tilt-backward",
+        "tilt-forward",
+      ],
+      accessories: ["earings", "flower", "glasses", "headphone"],
+      backgrounds: ["blue", "darkblue", "green", "grey", "red", "yellow"],
+    },
   },
+  // computed: {
+  //   selected: function () {
+
+  //   }
+  // }
 });
